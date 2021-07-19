1481409e2368bf57ce94edfbf96173b0b312fd2e
Evgeny Novgorodov <greatly01@ya.ru>
2021-07-19 20:46:10 +0500

    Initial commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Evgeny Novgorodov on 19.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
