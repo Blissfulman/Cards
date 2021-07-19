cc6b72fc2f26264345ad8f16ecda158360c22be0
Evgeny Novgorodov <greatly01@ya.ru>
2021-07-19 20:55:11 +0500

    Project structure was updated

--- Cards/Presentation/MainView.swift
+++ Cards/Presentation/MainView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  MainView.swift
 //  Cards
 //
 //  Created by Evgeny Novgorodov on 19.07.2021.
@@ -7,15 +7,15 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MainView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct MainView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MainView()
     }
 }
